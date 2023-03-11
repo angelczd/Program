6e5235c0ec1de087db818f14035192f14f3d732e
angelczd <angel_czd@163.com>
2023-03-12 05:38:33 +0800

    k

--- bootlog.c
+++ bootlog.c
@@ -9,4 +9,18 @@
 #define MAX_LINE_LEN 1024
 #define MAX_LOG_LEN 1024
 
+// Function: get_boot_time
+// Description: Get the boot time from the boot log file
+// Parameters:
+// 1. log_file: The boot log file
+// 2. boot_time: The boot time
+// Return: 0 on success, -1 on failure
 
+
+
+//hello world
+int main()
+{
+    printf("hello world");
+    return 0;
+}
