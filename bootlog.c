35920774098b901094485d9f0f9a4487cdf5833d
angelczd <angel_czd@163.com>
2023-03-12 05:36:50 +0800

    nothing added to commit but untracked files present (use "git add" to track)

---
+++ bootlog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define MAX_LINE_LEN 1024
+#define MAX_LOG_LEN 1024
+
+
