19b5bbb4a64145ce8713a6ec6f7d188ebe5ed37a
czd <angel_czd@163.com>
2023-03-22 00:50:23 +0800

    update test9.c

--- test9.c
+++ test9.c
@@ -7,13 +7,14 @@
 #define MAX_MODULE_NAME_LEN 256
 #define MAX_DEPENDENCIES_LEN 1024
 #define MAX_LOG_ENTRY_LEN 512
+#define MAX_LOG_FILE_PATH_LEN 128
 #define DEFAULT_LOG_ENTRIES_CAPACITY 500
 #define DEFAULT_LOG_FILE_PATH "./boot.log"
-#define LOG_ENTRIES_FLUSH_THRESHOLD 250
+#define DEFAULT_LOG_FLUSH_THRESHOLD 250
 
 typedef struct log_entry
 {
-    char timestamp[20];
+    char timestr[20];
     char module_name[MAX_MODULE_NAME_LEN];
     int seconds_from_first_log;
     char log_content[MAX_LOG_ENTRY_LEN];
@@ -30,37 +31,30 @@ typedef struct module_info
 
 typedef struct log_buffer
 {
-    log_entry_t **entries;
-    int capacity;
-    int count;
-    int read_index;
-    int write_index;
+    log_entry_t *entries;   //日志内容数组
+    int capacity;           //日志缓冲区容量
+    int count;              //当前存储日志数量(未保存文件的)
+    int read_index;         //读取日志的索引
+    int write_index;        //写入日志的索引
 } log_buffer_t;
 
-log_buffer_t g_log_buffer = {.entries = NULL, .capacity = 0, .count = 0, .read_index = 0, .write_index = 0};
+//日志缓冲区
+static log_buffer_t g_log_buffer = {.entries = NULL, .capacity = 0, .count = 0, .read_index = 0, .write_index = 0};
 
-module_info_t *g_module_list_head = NULL;
-module_info_t *g_module_list_tail = NULL;
+//模块链表头尾指针
+static module_info_t *g_module_list_head = NULL;
+static module_info_t *g_module_list_tail = NULL;
 
-char *log_file_path = NULL;
-int log_entries_capacity = DEFAULT_LOG_ENTRIES_CAPACITY;
-int log_entries_flush_threshold = LOG_ENTRIES_FLUSH_THRESHOLD;
+//日志文件路径
+static char g_log_file_path[MAX_LOG_FILE_PATH_LEN];
+//日志刷新阈值
+static int g_log_flush_threshold = DEFAULT_LOG_FLUSH_THRESHOLD;
 
-pthread_t g_writer_thread;
-// 递归锁, 允许同一个线程对同一个锁进行多次加锁，而不会造成死锁。
-pthread_mutex_t g_log_mutex = PTHREAD_MUTEX_RECURSIVE_INITIALIZER;
-pthread_cond_t g_log_cond = PTHREAD_COND_INITIALIZER;
+//日志写入线程
+static pthread_t g_writer_thread;
+//log_buffer写入互斥锁
+static pthread_mutex_t g_log_buf_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-int current_module_index = -1;
-
-char log_timestamp[20];
-
-void get_current_timestamp()
-{
-    time_t current_time;
-    time(&current_time);
-    strftime(log_timestamp, 20, "%Y-%m-%d %H:%M:%S", localtime(&current_time));
-}
 
 int get_seconds_from_first_log(const char *module_name)
 {
@@ -137,54 +131,38 @@ module_info_t *find_or_add_module_info(const char *module_name, const char *depe
 
 int init_log_buffer(int capacity, const char *log_path, int flush_threshold)
 {
-    g_log_buffer.entries = malloc(capacity * sizeof(log_entry_t *));
+    //init log file path
+    if(NULL != log_path)
+    {
+        snprintf(g_log_file_path, MAX_LOG_FILE_PATH_LEN, "%s", log_path);
+    }
+    else
+    {
+        snprintf(g_log_file_path, MAX_LOG_FILE_PATH_LEN, "%s", DEFAULT_LOG_FILE_PATH);
+    }
+    //init log flush threshold
+    g_log_flush_threshold = flush_threshold <= 0 ? DEFAULT_LOG_FLUSH_THRESHOLD : flush_threshold;
+
+    //init log buffer
+    g_log_buffer.entries = (log_entry_t*)malloc(capacity * sizeof(log_entry_t));
     if (g_log_buffer.entries == NULL)
     {
         fprintf(stderr, "Failed to allocate memory for log buffer.\n");
         return -1;
     }
-    g_log_buffer.capacity = capacity;
+    memset(g_log_buffer.entries, 0, capacity * sizeof(log_entry_t));
+    g_log_buffer.capacity = capacity <= 0 ? DEFAULT_LOG_ENTRIES_CAPACITY : capacity;
     g_log_buffer.count = 0;
     g_log_buffer.read_index = 0;
     g_log_buffer.write_index = 0;
 
-    log_file_path = malloc(strlen(log_path) + 1);
-    strcpy(log_file_path, log_path);
-
-    log_entries_capacity = capacity;
-    log_entries_flush_threshold = flush_threshold;
     return 0;
 }
 
 void print_to_log_buffer(const char *module_name, const char *dependencies, const char *log_content)
 {
-    get_current_timestamp();
-
-    if (current_module_index < 0 || strcmp(module_name, g_log_buffer.entries[current_module_index]->module_name) != 0)
-    {
-        current_module_index = -1;
-        for (int i = 0; i < g_log_buffer.count; i++)
-        {
-            if (strcmp(module_name, g_log_buffer.entries[i]->module_name) == 0)
-            {
-                current_module_index = i;
-                break;
-            }
-        }
-        if (current_module_index < 0)
-        {
-            current_module_index = g_log_buffer.write_index;
-            g_log_buffer.entries[current_module_index] = malloc(sizeof(log_entry_t));
-            if (g_log_buffer.entries[current_module_index] == NULL)
-            {
-                fprintf(stderr, "Failed to allocate memory for log entry.\n");
-                return;
-            }
-            strncpy(g_log_buffer.entries[current_module_index]->module_name, module_name, MAX_MODULE_NAME_LEN);
-            g_log_buffer.entries[current_module_index]->seconds_from_first_log = 0;
-            g_log_buffer.entries[current_module_index]->log_content[0] = '\0';
-        }
-    }
+    time_t current_time = time(NULL);
+    current_time = localtime(&current_time);
 
     // calculate seconds from first log
     int seconds = 0;
@@ -196,8 +174,7 @@ void print_to_log_buffer(const char *module_name, const char *dependencies, cons
             fprintf(stderr, "Failed to add module info.\n");
             return;
         }
-        time_t current_time;
-        time(&current_time);
+
         if (m->first_log_time == 0)
         {
             m->first_log_time = current_time;
@@ -210,65 +187,77 @@ void print_to_log_buffer(const char *module_name, const char *dependencies, cons
         m->last_log_time = current_time;
     }
 
-    g_log_buffer.entries[current_module_index]->seconds_from_first_log = seconds;
-    strncpy(g_log_buffer.entries[current_module_index]->timestamp, log_timestamp, 20);
-    strncpy(g_log_buffer.entries[current_module_index]->log_content, log_content, MAX_LOG_ENTRY_LEN);
-
-    if (current_module_index == g_log_buffer.write_index)
+    if(g_log_buffer.count < g_log_buffer.capacity)
     {
+        g_log_buffer.entries[g_log_buffer.write_index]->seconds_from_first_log = seconds;
+        strftime(g_log_buffer.entries[g_log_buffer.write_index]->timestr, 20, "%Y-%m-%d %H:%M:%S", current_time);
+        strncpy(g_log_buffer.entries[g_log_buffer.write_index]->log_content, log_content, MAX_LOG_ENTRY_LEN);
+
         g_log_buffer.write_index = (g_log_buffer.write_index + 1) % g_log_buffer.capacity;
         if (g_log_buffer.count < g_log_buffer.capacity)
         {
+            pthread_mutex_lock(&g_log_buf_mutex);
             g_log_buffer.count++;
+            pthread_mutex_unlock(&g_log_buf_mutex);
         }
         else
         {
             g_log_buffer.read_index = (g_log_buffer.read_index + 1) % g_log_buffer.capacity;
         }
-        if (g_log_buffer.count >= log_entries_flush_threshold)
-        {
-            pthread_mutex_lock(&g_log_mutex);
-            pthread_cond_signal(&g_log_cond);
-            pthread_mutex_unlock(&g_log_mutex);
-        }
     }
+    else
+    {
+        fprintf(stderr, "Log buffer is full!\n");
+    }
+    return;
 }
 
 void *writer_thread_func(void *arg)
 {
-    FILE *log_file = fopen(log_file_path, "a");
+    FILE *log_file = NULL; 
+
+    int count = 0;
+    int index = 0;
+
+    log_file =  fopen(g_log_file_path, "a");
     if (log_file == NULL)
     {
-        fprintf(stderr, "Failed to open log file: %s\n", log_file_path);
+        fprintf(stderr, "Failed to open log file: %s\n", g_log_file_path);
         return NULL;
     }
-    int flushed_count = 0;
+
     while (1)
     {
-        pthread_mutex_lock(&g_log_mutex);
-        pthread_cond_wait(&g_log_cond, &g_log_mutex);
-        int count = g_log_buffer.count;
-        int index = g_log_buffer.read_index;
-        for (int i = 0; i < count; i++)
+        sleep(1);
+
+        if (g_log_buffer.count >= g_log_flush_threshold)
         {
-            fprintf(log_file, "[%s][%s][%d]%s\n",
-                    g_log_buffer.entries[index]->timestamp,
-                    g_log_buffer.entries[index]->module_name,
-                    g_log_buffer.entries[index]->seconds_from_first_log,
-                    g_log_buffer.entries[index]->log_content);
-            flushed_count++;
-            index = (index + 1) % g_log_buffer.capacity;
-        }
-        fflush(log_file);
-        g_log_buffer.count -= count;
-        g_log_buffer.read_index = index;
-        pthread_mutex_unlock(&g_log_mutex);
-        if (flushed_count >= log_entries_capacity)
-        {
-            break;
+            pthread_mutex_lock(&g_log_buf_mutex);
+            count = g_log_buffer.count;
+            index = g_log_buffer.read_index;
+            pthread_mutex_unlock(&g_log_buf_mutex);
+
+            for (int i = 0; i < count; i++)
+            {
+                fprintf(log_file, "[%s][%s][%d]%s\n",
+                        g_log_buffer.entries[index]->timestr,
+                        g_log_buffer.entries[index]->module_name,
+                        g_log_buffer.entries[index]->seconds_from_first_log,
+                        g_log_buffer.entries[index]->log_content);
+
+                index = (index + 1) % g_log_buffer.capacity;
+            }
+            fflush(log_file);
+            
+            pthread_mutex_lock(&g_log_buf_mutex);
+            g_log_buffer.count -= count;
+            g_log_buffer.read_index = index;
+            pthread_mutex_unlock(&g_log_buf_mutex);
         }
     }
+
     fclose(log_file);
+
     return NULL;
 }
 
@@ -279,25 +268,24 @@ int start_log_writer_thread()
 
 void log_module_list()
 {
-    FILE *log_file = fopen(log_file_path, "a");
+    module_info_t *p = NULL;
+    FILE *log_file = fopen(g_log_file_path, "a");
     if (log_file == NULL)
     {
-        fprintf(stderr, "Failed to open log file: %s\n", log_file_path);
+        fprintf(stderr, "Failed to open log file: %s\n", g_log_file_path);
         return;
     }
-    module_info_t *p = g_module_list_head;
+    fprintf(log_file,"[%s][%s][%s][%.2lf]%s\n", "module", "first_log_time", "last_log_time", "diff", "dependencies");
+    p = g_module_list_head;
     while (p != NULL)
     {
-        if (p->last_log_time > 0)
-        {
-            double diff = difftime(p->last_log_time, p->first_log_time);
-            fprintf(log_file, "[%s][%s][%s][%.2lf]%s\n",
-                    p->module_name,
-                    asctime(localtime(&p->first_log_time)),
-                    asctime(localtime(&p->last_log_time)),
-                    diff,
-                    p->dependencies);
-        }
+        double diff = difftime(p->last_log_time, p->first_log_time);
+        fprintf(log_file, "[%s][%s][%s][%.2lf]%s\n",
+                p->module_name,
+                asctime(p->first_log_time),
+                asctime(p->last_log_time),
+                diff,
+                p->dependencies);
         p = p->next;
     }
     fclose(log_file);
@@ -307,8 +295,8 @@ void release_resources()
 {
     pthread_cancel(g_writer_thread);
     pthread_join(g_writer_thread, NULL);
-    pthread_mutex_destroy(&g_log_mutex);
-    pthread_cond_destroy(&g_log_cond);
+    pthread_mutex_destroy(&g_log_buf_mutex);
+
     for (int i = 0; i < g_log_buffer.capacity; i++)
     {
         if (g_log_buffer.entries[i] != NULL)
@@ -324,12 +312,11 @@ void release_resources()
         p = p->next;
         free(tmp);
     }
-    free(log_file_path);
 }
 
 int main()
 {
-    if (init_log_buffer(DEFAULT_LOG_ENTRIES_CAPACITY, DEFAULT_LOG_FILE_PATH, LOG_ENTRIES_FLUSH_THRESHOLD) != 0)
+    if (init_log_buffer(DEFAULT_LOG_ENTRIES_CAPACITY, DEFAULT_LOG_FILE_PATH, DEFAULT_LOG_ENTRIES_FLUSH_THRESHOLD) != 0)
     {
         fprintf(stderr, "Failed to initialize log buffer.\n");
         exit(EXIT_FAILURE);
